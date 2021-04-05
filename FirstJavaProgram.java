26cf50db55bf9969691b63d4476ec96a563672ea
MyMac <mymac@Joels-MacBook-Pro.local>
2021-04-05 09:07:47 -0500

    first commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,5 @@
+public class FirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Joel Okoh\nI am 35 years old\nI live in Houston, Texas.");
+    }
+}
